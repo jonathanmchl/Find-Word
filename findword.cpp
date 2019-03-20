6372ce6fe66f6dcc22af7069a2cf9e17496142f5
windydesty <48718419+windydesty@users.noreply.github.com>
2019-03-21 01:04:51 +0700

    Update findword.cpp

--- findword.cpp
+++ findword.cpp
@@ -132,6 +132,7 @@ int main ()
                              kata_ditemukan+=0;
                            }
                            cek=0;
+		
                            //Diagona kiri bawah
                            for(int k=0;k<p_kata;k++){
                              if(input[k]==puzzle[i+k][j-k]){
