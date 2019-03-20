020fbe055363238317259ccfb30f1bd410d78a30
hanaafriliza1012 <48717980+hanaafriliza1012@users.noreply.github.com>
2019-03-21 00:32:15 +0700

    Update findword.cpp

--- findword.cpp
+++ findword.cpp
@@ -149,32 +149,48 @@ int main ()
                              }
                               cek=0; 
                            
-                           //Diagonal kanan bawah
-                      for(int k=0;k<p_kata;k++){
-                           if(input[k]==puzzel[i+k][j+k]{
-                              cek=k;
-                           }
-                           else{
-                             break;
-                           }
-                           }
-                           if(cek==p_kata-1){
-                              kata_ditemukan+=1;
-                               }
-                            else{
-                              kata_ditemukan+=0;
-                            }
-                              cek=0;
-                                  }
-                                  }
-                                  }
-                           if(kata_ditemukan>0){
-                              cout<<"ADA";
-                           }
-                           else{
-                             cout<<"TIDAK ADA";
-                            }
-                         }
-                
-       
-     
+                           ///DIAGONAL KIRI BAWAH
+				for (int k=0;k<p_kata;k++){
+					if (input[k]==puzzle[i+k][j-k]){
+						cek=k;
+					}
+					else{
+						break;
+					}
+				}
+				if (cek==p_kata-1){
+					kata_ditemukan+=1;
+				}
+				else{
+					kata_ditemukan+=0;
+				}
+				cek=0;
+				
+				///DIAGONAL KANAN BAWAH
+				for (int k=0;k<p_kata;k++){
+					if (input[k]==puzzle[i+k][j+k]){
+						cek=k;
+					}
+					else{
+						break;
+					}
+				}
+				if (cek==p_kata-1){
+					kata_ditemukan+=1;
+				}
+				else{
+					kata_ditemukan+=0;
+				}
+				cek=0;
+				
+			}
+			
+		}
+	}
+	if(kata_ditemukan>0){
+		cout<<"ADA";
+	}
+	else {
+		cout<<"TIDAK ADA";
+	}
+}
